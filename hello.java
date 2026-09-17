e3840ffbd244cca04e53664571dfb23f7eac78af
Gopika262006 <seenivasagangopika2006@gmail.com>
2026-09-17 10:40:45 +0530

    feat : my first java program

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Gopika pro driver");
+String s = "Gopika";
+        for(int i = 0 ;i < s.length(); i++){
+
+            if(s.charAt(i) == 'G'){
+
+                System.out.println("Pro driver");
+            }
+        }
+    }
+}
