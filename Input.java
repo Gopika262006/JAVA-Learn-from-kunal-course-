ba908e3134ccdf82ecf872ed7ca39fcf621af231
Gopika262006 <seenivasagangopika2006@gmail.com>
2026-09-23 15:02:31 +0530

    FEAT : added a new main java

--- Input.java
+++ Input.java
@@ -5,6 +5,7 @@ public class Input {
         Scanner input = new Scanner(System.in);
         System.out.println("please enter some input:");
         int rollno = input.nextInt();
-        System.out.println("Your roll number is " + rollno);
+        System.out.println("Your roll number is  " + rollno);
     }
+
 }
